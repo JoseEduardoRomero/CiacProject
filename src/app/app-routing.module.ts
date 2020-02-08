02aa00573b40178b3cff2b9fd12aa20429550c5a
Eduardo <lalin03032000@gmail.com>
2020-02-07 22:40:07 -0600

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full' },
+  {path: 'login',component: LoginComponent},
+  {path: 'register',component: RegisterComponent},
+  {path: 'tabs',loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)}
+ 
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
